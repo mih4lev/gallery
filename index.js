ada211db6da9619d214c3f8bf117adcb7fbd89ae
Andrei Mikhalev <mikhalev@Mac-mini-Andrei.local>
2020-01-10 15:14:55 +0300

    heroku server changes

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.logger());
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
