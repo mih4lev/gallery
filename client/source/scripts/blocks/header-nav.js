84d4f88196e862cab4c8830cc933791462877403
Andrei Mikhalev <mikhalev@Mac-mini-Andrei.local>
2020-01-11 17:32:49 +0300

    create all adaptive for events page && start refactoring css

--- client/source/scripts/blocks/header-nav.js
+++ client/source/scripts/blocks/header-nav.js
@@ -2,7 +2,7 @@ export const headerNav = () => {
     const navBlock = document.querySelector(`.mainNavList`);
     document.addEventListener(`click`, (event) => {
         const windowSize = window.innerWidth;
-        if (windowSize > 768) return false;
+        if (windowSize > 1000) return false;
         const { target } = event;
         if (!target.classList.contains(`mainNav`)) {
             navBlock.style.display = `none`;
