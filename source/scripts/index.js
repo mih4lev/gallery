7b4e0c430226dc210b053079b3dbb7ad20a0d432
Andrei Mikhalev <andevme@gmail.com>
2020-01-23 23:28:08 +0300

    delete API comment

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -24,39 +24,3 @@ articleScroll();
 shareList();
 changePhotos();
 instagramPosts().catch((error) => console.log(error));
-
-// const data = {
-//     authorRU: `Екатерина Жуковская`,
-//     authorEN: `Ekaterina Zhukovskaya`,
-//     authorPhoto: `author_picture1.png`,
-//     authorAboutRU: `Художник тра тата такие дела там была пришла основала примеры работ позитивная любовь к красоте во всех проявлениях`,
-//     authorAboutEN: `The artist tra tata such things there was came based examples of works a positive love of beauty in all manifestations`,
-//     authorCityRU: `Москва, Россия`,
-//     authorCityEN: `Moscow, Russia`
-// };
-// const data = {
-//     authorRU: `Изабелла Лебедева`,
-//     authorEN: `Isabella Lebedeva`,
-//     authorPhoto: `author_picture2.png`,
-//     authorAboutRU: `Художник тра тата такие дела там была пришла основала примеры работ позитивная любовь к красоте во всех проявлениях`,
-//     authorAboutEN: `The artist tra tata such things there was came based examples of works a positive love of beauty in all manifestations`,
-//     authorCityRU: `Санкт-Петербург, Россия`,
-//     authorCityEN: `Saint-Petersburg, Russia`
-// };
-const data = {
-    authorRU: `Sasha Ignatiadouasd`,
-    authorEN: `Sasha Ignatiadouasd`,
-    authorPhoto: `author_picture33.png`,
-    authorAboutRU: `Художник тра тата такие дела там была пришла основала примеры работ позитивная любовь к красоте во всех проявлениях`,
-    authorAboutEN: `The artist tra tata such things there was came based examples of works a positive love of beauty in all manifestations`,
-    authorCityRU: `Düsseldorf, Germany`,
-    authorCityEN: `Düsseldorf, Germany`
-};
-const options = {
-    method: `PUT`,
-    headers: {
-        'Content-Type': `application/json;charset=utf-8`
-    },
-    body: JSON.stringify(data)
-};
-fetch(`/api/authors/11`, options);
