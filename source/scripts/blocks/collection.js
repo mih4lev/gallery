7da1e46487d810f038ab23e18b3b247a2f001b58
Andrei Mikhalev <andevme@gmail.com>
2020-02-06 15:30:19 +0300

    fix filters loader

--- source/scripts/blocks/collection.js
+++ source/scripts/blocks/collection.js
@@ -179,7 +179,6 @@ const changeFilters = (pictures) => {
                 target.classList.contains(`filterLabel`) ||
                 target.classList.contains(`filters`)
             ) return false;
-            console.log(target);
             //
             // orientation
             let orientation = [];
