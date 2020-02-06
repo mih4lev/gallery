3724e929ff3e9c6377139a3cab38365fa1601316
Andrei Mikhalev <andevme@gmail.com>
2020-02-06 18:46:14 +0300

    creating admin login form

--- models/db.model.js
+++ models/db.model.js
@@ -8,6 +8,14 @@ const mysqlOptions = {
     port: 3306
 };
 
+// const mysqlOptions = {
+//     host: "localhost",
+//     user: "root",
+//     password: "i8nvl551s",
+//     database: "gallery",
+//     port: 3306
+// };
+
 const requestDB = async (query) => {
     const connection = await mysql.createConnection(mysqlOptions);
     const response = await connection.query(query);
