530e607629dd74b3a61bc9e0a7e64e2d1a0ef860
Andrei Mikhalev <andevme@gmail.com>
2020-02-07 16:46:34 +0300

    complete gallery view

--- source/scripts/blocks/picture.js
+++ source/scripts/blocks/picture.js
@@ -16,7 +16,7 @@ export const changePhotos = () => {
     }, 300);
     if (!mainPicture || !thumbs.length) return false;
     const tempPhoto = new Image();
-    const changePhoto = (picture, original) => {
+    const changePhoto = (picture, original, position) => {
         return () => {
             mainPicture.style.opacity = `0`;
             tempPhoto.addEventListener(`load`, () => {
@@ -24,6 +24,7 @@ export const changePhotos = () => {
                 setTimeout(() => {
                     mainPicture.style.opacity = `1`;
                     mainPicture.dataset.original = original;
+                    mainPicture.dataset.position = position;
                 }, 200);
             });
             setTimeout(() => {
@@ -31,17 +32,81 @@ export const changePhotos = () => {
             }, 200);
         };
     };
-    thumbs.forEach((thumb) => {
+    thumbs.forEach((thumb, index) => {
         const { dataset: { picture, original }} = thumb;
-        thumb.addEventListener(`click`, changePhoto(picture, original))
+        thumb.addEventListener(`click`, changePhoto(picture, original, index + 1))
     });
 };
+
+const checkVisible = (button, isHidden) => {
+    button.style.opacity = (isHidden) ? `0` : `1`;
+    button.style.cursor = (isHidden) ? `default` : `pointer`;
+};
+
+const showPhoto = (gallery, galleryNode, position) => {
+    const isFirst = (position === 1);
+    const isLast = (position === gallery.length);
+    const photoLink = gallery[position - 1];
+    const photoNode = galleryNode.querySelector(`.originalPhoto`);
+    photoNode.addEventListener(`load`, () => {
+        photoNode.style.opacity = `1`;
+    });
+    photoNode.src = photoLink;
+    photoNode.dataset.position = position;
+    const leftButton = galleryNode.querySelector(`.moveArrow--left`);
+    checkVisible(leftButton, isFirst);
+    const rightButton = galleryNode.querySelector(`.moveArrow--right`);
+    checkVisible(rightButton, isLast);
+};
+
+const arrowClickHandler = (photoNode, gallery, galleryNode) => {
+    return ({ target }) => {
+        const { dataset: { position }} = photoNode;
+        const currentPosition = Number(position);
+        const isLeft = (target.classList.contains(`moveArrow--left`));
+        const newPosition = (isLeft) ? currentPosition - 1 : currentPosition + 1;
+        if (!gallery[newPosition - 1]) return false;
+        photoNode.style.opacity = `0`;
+        showPhoto(gallery, galleryNode, newPosition);
+    }
+};
+
+const cloneGalleryTemplate = (gallery, position) => {
+    const bodyNode = document.querySelector(`body`);
+    const galleryTemplate = document.querySelector(`.pictureGallery`);
+    const templateClone = galleryTemplate.content.cloneNode(true);
+    const galleryNode = templateClone.querySelector(`.galleryWrapper`);
+    const photoNode = galleryNode.querySelector(`.originalPhoto`);
+    const arrowHandler = arrowClickHandler(photoNode, gallery, galleryNode);
+    const leftButton = galleryNode.querySelector(`.moveArrow--left`);
+    const rightButton = galleryNode.querySelector(`.moveArrow--right`);
+    leftButton.addEventListener(`click`, arrowHandler);
+    rightButton.addEventListener(`click`, arrowHandler);
+    const closeButton = galleryNode.querySelector(`.closeButton`);
+    closeButton.addEventListener(`click`, () => {
+        bodyNode.removeChild(galleryNode);
+    });
+    showPhoto(gallery, galleryNode, position);
+    bodyNode.appendChild(galleryNode);
+    return galleryNode;
+};
+
+const thumbClickHandler = (gallery) => {
+    return ({ target }) => {
+        const windowSize = window.innerWidth;
+        if (windowSize < 768) return false;
+        const { dataset: { position }} = target;
+        cloneGalleryTemplate(gallery, Number(position));
+    };
+};
+
+const receiveOriginalURL = ({ dataset: { original }}) => original;
 
 export const showGallery = () => {
     const photoWrapper = document.querySelector(`.picturePhotoWrapper`);
     const mainPicture = document.querySelector(`.picturePhoto`);
     if (!photoWrapper || !mainPicture) return false;
-    mainPicture.addEventListener(`click`, () => {
-
-    });
+    const possiblePhotoNodes = [...document.querySelectorAll(`.interestPhoto`)];
+    const gallery = possiblePhotoNodes.map(receiveOriginalURL);
+    mainPicture.addEventListener(`click`, thumbClickHandler(gallery));
 };
