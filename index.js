71779481837d99c4bbf7419418d5ae779627a099
mikhalev <andevme@gmail.com>
2020-04-22 11:48:25 +0300

    add yandex counter && robots.txt

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const corsOptions = require("./middlewares/cors.middleware");
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Test4');
+    response.send('Test5');
 });
 
 // compress response
@@ -37,30 +37,30 @@ app.use(require(`./middlewares/admin.middleware`));
 app.use('/api', cors(corsOptions));
 // // routes
 // app.use('/', require('./routes/home.routes'));
-// app.use('/collection', require('./routes/collection.routes'));
-// app.use('/authors', require('./routes/authors.routes'));
-// app.use('/events', require('./routes/events.routes'));
-// app.use('/delivery', require('./routes/delivery.routes'));
-// app.use('/basket', require('./routes/basket.routes'));
-// app.use('/orders', require('./routes/orders.routes'));
-// app.use('/404', require('./routes/404.routes'));
-// app.use('/api', require('./routes/api.routes'));
-// app.use('/api/options', require('./routes/api.options.routes'));
-// app.use('/api/pages', require('./routes/api.pages.routes'));
-// app.use('/api/language', require('./routes/api.language.routes'));
-// app.use('/api/collection', require('./routes/api.collection.routes'));
-// app.use('/api/categories', require('./routes/api.categories.routes'));
-// app.use('/api/events', require('./routes/api.events.routes'));
-// app.use('/api/authors', require('./routes/api.authors.routes'));
-// app.use('/api/rewards', require('./routes/api.rewards.routes'));
-// app.use('/api/educations', require('./routes/api.educations.routes'));
-// app.use('/api/exhibitions', require('./routes/api.exhibitions.routes'));
-// app.use('/api/colors', require('./routes/api.colors.routes'));
-// app.use('/api/genres', require('./routes/api.genres.routes'));
-// app.use('/api/techniques', require('./routes/api.techniques.routes'));
-// app.use('/api/photos', require('./routes/api.photos.routes'));
-// app.use('/api/pictures', require('./routes/api.pictures.routes'));
-// app.use('/api/orders', require('./routes/api.orders.routes'));
+app.use('/collection', require('./routes/collection.routes'));
+app.use('/authors', require('./routes/authors.routes'));
+app.use('/events', require('./routes/events.routes'));
+app.use('/delivery', require('./routes/delivery.routes'));
+app.use('/basket', require('./routes/basket.routes'));
+app.use('/orders', require('./routes/orders.routes'));
+app.use('/404', require('./routes/404.routes'));
+app.use('/api', require('./routes/api.routes'));
+app.use('/api/options', require('./routes/api.options.routes'));
+app.use('/api/pages', require('./routes/api.pages.routes'));
+app.use('/api/language', require('./routes/api.language.routes'));
+app.use('/api/collection', require('./routes/api.collection.routes'));
+app.use('/api/categories', require('./routes/api.categories.routes'));
+app.use('/api/events', require('./routes/api.events.routes'));
+app.use('/api/authors', require('./routes/api.authors.routes'));
+app.use('/api/rewards', require('./routes/api.rewards.routes'));
+app.use('/api/educations', require('./routes/api.educations.routes'));
+app.use('/api/exhibitions', require('./routes/api.exhibitions.routes'));
+app.use('/api/colors', require('./routes/api.colors.routes'));
+app.use('/api/genres', require('./routes/api.genres.routes'));
+app.use('/api/techniques', require('./routes/api.techniques.routes'));
+app.use('/api/photos', require('./routes/api.photos.routes'));
+app.use('/api/pictures', require('./routes/api.pictures.routes'));
+app.use('/api/orders', require('./routes/api.orders.routes'));
 // // 404
 // app.use((request, response, next) => {
 //     const { url } = request;
