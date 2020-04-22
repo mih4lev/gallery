64108389d4fac1fdc443590cbd2375930d52f19e
mikhalev <andevme@gmail.com>
2020-04-22 11:59:36 +0300

    test robots visible

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -8,29 +8,30 @@ const router = new Router();
 
 router.get(`/`, async (request, response) => {
     const pageLink = `home`;
-    const data = await collectData(request, pageLink);
+    // const data = await collectData(request, pageLink);
     // const lang = data.language;
-    // await Promise.all([
-    //     data.collection = await requestLangCollectionList(lang),
-    //     data.authors = await requestLanguageAuthors(lang, 3),
-    //     data.events = await requestLanguageEvents(lang, 3)
-    // ]);
-    // data.collection.forEach((picture) => {
-    //     picture.isAdmin = request.isAdmin;
-    // });
-    // data.isSingleAuthor = (data.authors.length === 1);
-    // // data.isSingleAuthor = true;
-    // if (data.isSingleAuthor) {
-    //     data.authors = data.authors[0];
-    //     data.authors.pictures.forEach((picture) => {
-    //         picture.authorID = data.authors.authorID;
-    //     });
-    // }
-    // data.events.forEach((event, index) => {
-    //     if (index === 1) event.isActive = true;
-    //     event.hasPhoto = (event.eventPhoto !== `NULL` && event.eventPhoto !== null);
-    // });
-    // response.render(pageLink, data);
+    const lang = `ru`;
+    await Promise.all([
+        data.collection = await requestLangCollectionList(lang),
+        data.authors = await requestLanguageAuthors(lang, 3),
+        data.events = await requestLanguageEvents(lang, 3)
+    ]);
+    data.collection.forEach((picture) => {
+        picture.isAdmin = request.isAdmin;
+    });
+    data.isSingleAuthor = (data.authors.length === 1);
+    // data.isSingleAuthor = true;
+    if (data.isSingleAuthor) {
+        data.authors = data.authors[0];
+        data.authors.pictures.forEach((picture) => {
+            picture.authorID = data.authors.authorID;
+        });
+    }
+    data.events.forEach((event, index) => {
+        if (index === 1) event.isActive = true;
+        event.hasPhoto = (event.eventPhoto !== `NULL` && event.eventPhoto !== null);
+    });
+    response.render(pageLink, data);
 });
 
 module.exports = router;
